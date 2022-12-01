61de7c825d685f2b808fa54510ba8dbc87752866
Joseph Campuzano <campuzanojoseph@gmail.com>
2022-12-01 09:19:50 -0600

    day 1

--- 01/index.js
+++ 01/index.js
@@ -1,12 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 
-const partOne = (input) => {
-  return input;
+const partOne = (input = '') => {
+  return Math.max(
+    ...input.split('\n\n').map((chunk) =>
+      chunk
+        .split('\n')
+        .map((line) => Number(line))
+        .reduce((acc, n) => acc + n, 0)
+    )
+  );
 };
 
-const partTwo = (input) => {
-  return input;
+const partTwo = (input = '') => {
+  return input
+    .split('\n\n')
+    .map((chunk) =>
+      chunk
+        .split('\n')
+        .map((line) => Number(line))
+        .reduce((acc, n) => acc + n, 0)
+    )
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((acc, n) => acc + n);
 };
 
 const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf-8');
